c630962336810aad72f14110aa5592aaa4dd7579
Sanat <sanatc@jcsoftwaresolution.com>
2020-12-09 00:26:59 +0530

    changes

---
+++ count/count.js
@@ -0,0 +1,37 @@
+
+var allIncrement = document.querySelectorAll('button.additems')
+var integersdiv = document.querySelectorAll('div[id=number]')
+var carddiv = document.querySelectorAll('div[class=cartbtn]')
+console.log(allIncrement, integersdiv)
+var values= [0,0,0,0]
+// let int = document.getElementById('number');
+
+// //function for increment
+function additems(index){
+    // integer +=1;
+    console.log(index)
+    values[index]+=1 
+    const integer = values[index]
+    integersdiv[index].innerHTML = integer;
+}
+
+//function for decrement
+function decitems(index){
+    values[index]-=1 
+   const  integer =  values[index] 
+    integersdiv[index].innerHTML = integer;
+}
+function deleteitems(index){
+    integersdiv[index].remove()
+    carddiv[index].remove()
+   const countheader= document.querySelectorAll('button[class=countbtn]')[0].innerText-1
+   document.querySelectorAll('button[class=countbtn]')[0].innerText = countheader
+}
+// function for refreshing the content
+function refresh(){
+ location.reload();
+
+}
+function headercount(){
+
+}
